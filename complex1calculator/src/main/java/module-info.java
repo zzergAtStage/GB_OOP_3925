6656963cd7876bff973497976939c0f6372d5275
Sergio Brü <brusentsovsv@gmail.com>
2023-06-21 12:20:24 +0200

    Added S07-1 project

---
+++ complex1calculator/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module oop.seminars.s071.complex1calculator {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens oop.seminars.s071.complex1calculator to javafx.fxml;
+    exports oop.seminars.s071.complex1calculator;
+}
